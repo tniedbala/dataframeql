4e3f50bd8f653b2075e158b40b399cc40ba81fea
Tim Niedbala <tniedbala@live.com>
2022-10-23 21:29:26 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='dataframeql',
+    version='0.0.1',
+    description='Python Pipeline Syntax',
+    author='Tim Niedbala',
+    author_email='tniedbala@live.com',
+    url='',
+    packages=['dataframeql'],
+)
